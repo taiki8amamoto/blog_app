7becfd2a165863864d667633b0c1a6e576bdb5ea
taiki yamamoto <ultra0115.20@gmail.com>
2023-07-19 18:03:57 +0900

    実装完了

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,22 +1,53 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def new
+    @blog = Blog.new
   end
 
   def create
+    @blog = Blog.new(blog_params)
+    if params[:back]
+      render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを作成しました。"
+      else
+        render :new
+      end
+    end
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
 
   def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: 'ブログを更新しました。'
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
+
+  def confirm
+    @blog = Blog.new(blog_params)
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :blogs do
+    collection do
+      post :confirm
+    end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
